090ed5e783a4dc5f36a9c23df82945bf56284a9a
Aaron Watters <awatters@simonsfoundation.org>
2016-07-28 14:14:04 -0400

    comment out non-working package

--- inferelator_ng/R_code/packages.R
+++ inferelator_ng/R_code/packages.R
@@ -3,6 +3,6 @@
 install.packages('inline', repos='http://cran.us.r-project.org')
 install.packages('parallel', repos='http://cran.us.r-project.org')
 #install.packages('corpcor', repos='http://cran.us.r-project.org') -- doesn't work on travis
-install.packages("corpcor", dependencies=TRUE, repos='http://cran.rstudio.com/')
+#install.packages("corpcor", dependencies=TRUE, repos='http://cran.rstudio.com/') -- neither does this
 # This doesn't work on travis:
 # install.packages('ggplot2', repos='http://cran.us.r-project.org')
